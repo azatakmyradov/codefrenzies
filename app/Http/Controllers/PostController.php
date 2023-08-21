cafec56e2c381a31518205283068e34ef4620b04
Azat Akmyradov <azat@akmyradov.me>
2023-08-21 11:52:52 -0400

    add infinite scroll feature (#2)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,24 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
-        $posts = Cache::rememberForever('posts.published', function () {
-            return Post::whereNot('published_at', null)->latest('published_at')->get();
+        $page = request()->has('page') ? request('page') : 1;
+
+        $posts = Cache::rememberForever("posts.published.{$page}", function () {
+            return Post::whereNot('published_at', null)->latest('published_at')->paginate(8);
         });
+
+        // dd($posts);
 
         return Inertia::render('Posts/Index', [
             'posts' => $posts
         ]);
     }
 
-    public function show($slug)
+    public function show($slug): Response
     {
         $post = Cache::rememberForever("posts.{$slug}", function () use ($slug) {
            return Post::where('slug', $slug)->with('category')->first();
